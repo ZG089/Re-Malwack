a9df10b65d7cb84521547d65a5bd261b438ab80e
ZG089 <159061718+ZG089@users.noreply.github.com>
2024-09-17 12:05:20 +0000

    Drop bundled curl and mv

--- customize.sh
+++ customize.sh
@@ -48,8 +48,6 @@ set_permissions() {
   :
   set_perm_recursive $MODPATH/system/etc/hosts 0 0 0644 0755
   set_perm  $MODPATH/system/bin/re-malwack  0  0  0777
-  set_perm  $MODPATH/system/bin/curl  0  0  0777
-  set_perm  $MODPATH/system/bin/mv  0  0  0777
   set_perm  $MODPATH/system/bin/sed  0  0  0777
 }
 
