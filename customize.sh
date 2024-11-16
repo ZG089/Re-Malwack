04467f05df890daca1352abfd1d4bf7282aefdae
ZG_DEV <159061718+ZG089@users.noreply.github.com>
2024-11-16 23:18:16 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -52,6 +52,11 @@ set_permissions() {
 # MMT Extended Logic - Don't modify anything after this
 ##########################################################################################
 
+# ok.
+DO_WE_HAVE_ANYTHING_TO_EXTRACT=false
+DO_WE_REALLY_NEED_ADDONS=false
 SKIPUNZIP=1
 unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
-. $TMPDIR/functions.sh
+unzip -qjo "$ZIPFILE" 'common/install.sh' -d $TMPDIR >&2
+source $TMPDIR/functions.sh
+. $TMPDIR/install.sh
