c96e3d997d6220797ddd9531132f156bad285d5b
Luna <luna.realm.io.bennett24@outlook.com>
2024-11-17 01:24:15 -0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -3,7 +3,7 @@ REPLACE="/system/etc/hosts
 /system/etc/hosts.tmp
 "
 DO_WE_HAVE_ANYTHING_TO_EXTRACT=false
-DO_WE_REALLY_NEED_ADDONS=true
+DO_WE_REALLY_NEED_ADDONS=false
 SKIPUNZIP=1
 for i in "common/functions.sh" "common/install.sh" "system"; do
   unzip -qjo "$ZIPFILE" $i -d $TMPDIR || abort "- Failed to unpack module resources, please try again"
