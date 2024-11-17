447325fb87f693a4672bd00f1d2f43b519b1cd28
Luna <luna.realm.io.bennett24@outlook.com>
2024-11-17 03:10:50 -0800

    Update: small change.

--- customize.sh
+++ customize.sh
@@ -1,11 +1,8 @@
-# who tf wrote this lmao, should have made this as an array lmao.
-REPLACE="/system/etc/hosts
-/system/etc/hosts.tmp
-"
+REPLACE=""
 DO_WE_HAVE_ANYTHING_TO_EXTRACT=false
 DO_WE_REALLY_NEED_ADDONS=false
 SKIPUNZIP=1
-for i in "common/functions.sh" "common/install.sh" "system"; do
+for i in "common" "system"; do
   unzip -qjo "$ZIPFILE" $i -d $TMPDIR || abort "- Failed to unpack module resources, please try again"
 done
 source $TMPDIR/functions.sh
