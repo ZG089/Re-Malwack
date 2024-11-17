803b8423c1b19fe9b0ad67bfc486a328c9a2111e
Luna <luna.realm.io.bennett24@outlook.com>
2024-11-17 01:13:32 -0800

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -17,8 +17,8 @@ ui_print "- Checking internet connection..."
 
 # let's check do we have internet or not.
 if ! ping -w 1 google.com; then
-	ui_print "- This module requires internet connection to download"
-	abort "  Some utilities, please connect to a mobile network and try again."
+    ui_print "- This module requires internet connection to download"
+    abort "  Some utilities, please connect to a mobile network and try again."
 fi
 
  # Download the hosts file and save it as "hosts"
@@ -28,22 +28,24 @@ wget "https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.
 wget "https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist/master/hosts/hosts2" $TMPDIR/hosts2
 wget "https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist/master/hosts/hosts3" $TMPDIR/hosts3
 cat $TMPDIR/hosts0 $TMPDIR/hosts1 $TMPDIR/hosts2 $TMPDIR/hosts3 | sort | uniq > $TMPDIR/hosts
-# cleaning up
-rm $TMPDIR/hosts0
-rm $TMPDIR/hosts1
-rm $TMPDIR/hosts2
-rm $TMPDIR/hosts3 
 
 # let's see if the file was downloaded or not.
 if [ ! -f "$TMPDIR/hosts" ]; then
-	abort "- The file wasn't downloaded, please try again."
+    abort "- The file wasn't downloaded, please try again."
 else 
-	ui_print "- The new hosts file is downloaded successfully ✓"
+    ui_print "- The new hosts file is downloaded successfully ✓"
 fi
 
 ui_print "- Currently protecting a/an $(getprop ro.product.brand) device, model: $(getprop ro.product.model) 🛡"
 ui_print "- Installing hosts file"
 cat $TMPDIR/hosts /etc/hosts | sort | uniq > $MODPATH/system/etc/hosts
-#set perms
-chmod 0644 $MODPATH/system/etc/hosts
-chmod 0777 $MODPATH/system/bin/rmlwk
+chown 0 $MODPATH/system/bin/rmlwk $MODPATH/system/etc/hosts
+chgrp 0 $MODPATH/system/bin/rmlwk $MODPATH/system/etc/hosts
+chmod 644 $MODPATH/system/etc/hosts
+chmod 755 $MODPATH/system/bin/rmlwk
+
+# nuke other things.
+rm $TMPDIR/hosts0
+rm $TMPDIR/hosts1
+rm $TMPDIR/hosts2
+rm $TMPDIR/hosts3 
