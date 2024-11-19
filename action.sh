c1b3a1d3f7ffed6fc3ab2c89f7df3173c6221db2
ZG_DEV <159061718+ZG089@users.noreply.github.com>
2024-11-19 22:59:59 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -6,20 +6,20 @@ ui_print "
   ╚────────────────────────────────────────╝
 "
 sleep 0.5
-ui_print "       Updating Defenses...."
+echo "       Updating Defenses...."
 if ! ping -w 3 google.com &>/dev/null; then
 
-    ui_print "       This module requires internet connection to download"
+    echo "       This module requires internet connection to download"
     abort "        Some utilities, please connect to a mobile network and try again."
 fi
 # Download the hosts file and save it as "hosts"
-wget https://hosts.ubuntu101.co.za/hosts
+wget data/local/tmp/hosts https://hosts.ubuntu101.co.za/hosts
 
 # merge bombs to get a big nuke
 mkdir -p $MODPATH/system/etc
-ui_print "       Preparing New weapons🔫..."
+echo "       Preparing New weapons🔫..."
 {
-    for j_cole in /system/etc/hosts hosts; do
+    for j_cole in /system/etc/hosts data/local/tmp/hosts; do
         cat $j_cole
         echo ""
     done
@@ -29,6 +29,6 @@ ui_print "       Preparing New weapons🔫..."
 if [ ! -f "hosts" ]; then
     abort "       Looks like there is a problem with some weapons, maybe check your internet connection?"
 else 
-    ui_print "       Everthing is fine now, Enjoy 😉"
+    echo "       Everthing is fine now, Enjoy 😉"
     sleep 0.5
 fi
