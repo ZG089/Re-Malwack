661ba763329c07efd5682d7a403d1c619495476b
ZGX089 <159061718+ZG089@users.noreply.github.com>
2024-12-19 16:09:56 +0200

    `Update action.sh and common/install.sh with minor formatting changes and wording adjustments.`

--- action.sh
+++ action.sh
@@ -1,9 +1,9 @@
 echo "
-╔────────────────────────────────────────╗
-│░█▀▄░█▀▀░░░░░█▄█░█▀█░█░░░█░█░█▀█░█▀▀░█░█│
-│░█▀▄░█▀▀░▄▄▄░█░█░█▀█░█░░░█▄█░█▀█░█░░░█▀▄│
-│░▀░▀░▀▀▀░░░░░▀░▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀│
-╚────────────────────────────────────────╝
+  ╔────────────────────────────────────────╗
+  │░█▀▄░█▀▀░░░░░█▄█░█▀█░█░░░█░█░█▀█░█▀▀░█░█│
+  │░█▀▄░█▀▀░▄▄▄░█░█░█▀█░█░░░█▄█░█▀█░█░░░█▀▄│
+  │░▀░▀░▀▀▀░░░░░▀░▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀│
+  ╚────────────────────────────────────────╝
 "
 sleep 0.5
 echo "       Upgrading Defenses🛡️, this may take a while...."
@@ -17,7 +17,7 @@ fi
         wget -O /sdcard/hosts1 https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts #122k hosts
         wget -O /sdcard/hosts2 https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/pro.plus-compressed.txt
         wget -O /sdcard/hosts3 https://hblock.molinero.dev/hosts # 458k hosts
-echo "- Preparing New weapons🔫..."
+echo "       Preparing New weapons🔫..."
 {
     for j_cole in /system/etc/hosts /sdcard/hosts1 /sdcard/hosts2 /sdcard/hosts3 ; do
         cat $j_cole
@@ -27,7 +27,7 @@ echo "- Preparing New weapons🔫..."
 
 # let's see if the file was downloaded or not.
 if [ ! -f "/sdcard/hosts3" ]; then
-    echo "- Looks like there is a problem with some weapons, maybe check your internet connection?"
+    echo "       Looks like there is a problem with some weapons, maybe check your internet connection?"
 else 
     echo "       Everthing is fine now, Enjoy 😉"
     rm /sdcard/hosts*
