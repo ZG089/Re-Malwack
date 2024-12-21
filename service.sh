ebe27f0e8be652869266b29e50ec0d98b0029b35
ZGX089 <159061718+ZG089@users.noreply.github.com>
2024-12-21 22:22:47 +0200

    rmlwk: filter out comments and empty lines when updating hosts file +service.sh : improve code

--- service.sh
+++ service.sh
@@ -1,3 +1,15 @@
+#!/system/bin/sh
+
+# Variables
 MODDIR="/data/adb/modules/Re-Malwack"
-string="description=Status: Protection is enabled ✅ | protection update date: $(date)"
-sed -i "s/^description=.*/$string/g" $MODDIR/module.prop
+HOSTS_FILE="/system/etc/hosts"
+string="description=Status: Protection is enabled ✅ | Protection update date: $(date)"
+
+# Check if hosts file contains blocked entries
+if grep -q '0.0.0.0' "$HOSTS_FILE"; then
+    # Update the module description
+    sed -i "s/^description=.*/$string/g" "$MODDIR/module.prop"
+else
+    # Update the module description if no entries are blocked
+    sed -i "s/^description=.*/description=Status: disabled due to no blocked entries ❌/g" "$MODDIR/module.prop"
+fi
