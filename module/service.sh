b13a2e7c465c8a9ede8f8d323d4430bf83534034
ZG_DEV <testfolatic@gmail.com>
2025-02-11 18:22:11 +0200

    module_wide: introducing logging system

--- module/service.sh
+++ module/service.sh
@@ -1,15 +1,23 @@
 #!/system/bin/sh
 
+# Logging function
+function log_message() {
+    local message="$1"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> "/sdcard/Re-Malwack/logs.txt"
+}
 # Variables
 MODDIR="/data/adb/modules/Re-Malwack"
 HOSTS_FILE="/system/etc/hosts"
-string="description=Status: Protection is enabled ✅ | Protection update date: $(date)"
+string="description=Status: Protection is enabled ✅ | Last updated: $(date)"
 
 # Check if hosts file contains blocked entries
 if grep -q '0.0.0.0' "$HOSTS_FILE"; then
     # Update the module description
     sed -i "s/^description=.*/$string/g" "$MODDIR/module.prop"
+    log_message "Protection is enabled ✅ | Last updated: $(date)"
+
 else
     # Update the module description if no entries are blocked
-    sed -i "s/^description=.*/description=Status: disabled due to no blocked entries ❌/g" "$MODDIR/module.prop"
+    sed -i "s/^description=.*/description=Status: Protection disabled due to no blocked entries ❌/g" "$MODDIR/module.prop"
+    log_message "Status: Protection disabled due to no blocked entries"
 fi
