3e9e85aaff61e05241a21ba531873fe6a09f8543
ZGX089 <159061718+ZG089@users.noreply.github.com>
2025-02-23 00:41:40 +0200

    service.sh: Added module disable detection

--- module/service.sh
+++ module/service.sh
@@ -17,7 +17,14 @@ if grep -q '0.0.0.0' "$HOSTS_FILE"; then
     log_message "Protection is enabled ✅ | Last updated: $(date)"
 
 else
-    # Update the module description if no entries are blocked
-    sed -i "s/^description=.*/description=Status: Protection disabled due to no blocked entries ❌/g" "$MODDIR/module.prop"
-    log_message "Status: Protection disabled due to no blocked entries"
+    if [ -f "$MODDIR/disable" ]; then
+        log_message "Status: Module is disabled"
+        string="description=status: Disabled ❌ | $(date)"
+        sed -i "s/^description=.*/$string/g" "$MODDIR/module.prop"
+        return  # Exit the else block early
+    else
+        # Update the module description if no entries are blocked
+        sed -i "s/^description=.*/description=Status: Protection disabled due to no blocked entries ❌/g" "$MODDIR/module.prop"
+        log_message "Status: Protection disabled due to no blocked entries"
+    fi
 fi
