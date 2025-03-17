eb98fadff661f7be0ad48c4549dce01a4fb7e37d
ZGX089 <159061718+ZG089@users.noreply.github.com>
2025-03-17 17:52:41 +0200

    repo: Lots of changes
    - module: new website
    - rmlwk: remove extra hosts file reset code
    - rmlwk: more logging
    - rmlwk: script_integration: Added module update check in `update_status()` func
    - rmlwk: script_integration: log install status (update/first-time install)
    workflow: added more keywords to be excluded
    gitignore: update

---
+++ website/app/layout.tsx
@@ -0,0 +1,32 @@
+import type React from "react"
+import "@/app/globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Re-Malwack",
+  description: "A powerful module for protecting your device's security",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
+
+
+import './globals.css'
