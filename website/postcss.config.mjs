5cb5e86a278aba526796aad00e9bcb0db10a46c1
ZG089 <testfolatic@gmail.com>
2025-03-18 06:22:27 +0200

    website: reset changes

--- website/postcss.config.mjs
+++ website/postcss.config.mjs
@@ -6,15 +6,3 @@ const config = {
 };
 
 export default config;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/Re-Malwack.github.io",
-  assetPrefix: "/Re-Malwack.github.io/",
-};
-
-export default nextConfig;
