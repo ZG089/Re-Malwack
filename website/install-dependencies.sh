5c2cced1f750658902e296299458fca277161207
ZG089 <testfolatic@gmail.com>
2025-03-18 06:02:06 +0200

    website: dependecies

---
+++ website/install-dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install all required shadcn/ui dependencies
+npm install @radix-ui/react-accordion @radix-ui/react-alert-dialog @radix-ui/react-aspect-ratio @radix-ui/react-avatar @radix-ui/react-checkbox @radix-ui/react-dialog @radix-ui/react-dropdown-menu @radix-ui/react-label @radix-ui/react-navigation-menu @radix-ui/react-popover @radix-ui/react-select @radix-ui/react-separator @radix-ui/react-slot @radix-ui/react-tabs @radix-ui/react-toast class-variance-authority clsx lucide-react next-themes tailwind-merge tailwindcss-animate
