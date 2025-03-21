2c1b9155b29c10e5de71bdfa2769376287fe3e17
ZG089 <testfolatic@gmail.com>
2025-03-21 14:34:56 +0200

    customize.sh: don't add/set blocklist toggles to 0 if module is already installed

--- module/customize.sh
+++ module/customize.sh
@@ -64,9 +64,11 @@ config_file="/data/adb/Re-Malwack/config.sh"
 types="block_porn block_gambling block_fakenews block_social daily_update"
 mkdir -p "/data/adb/Re-Malwack"
 touch "$config_file"
-for type in $types; do
-    grep -q "^$type=" "$config_file" || echo "$type=0" >> "$config_file"
-done
+if [ ! -d "/data/adb/modules/Re-Malwack" ]; then # if the module directory doesn't exist (first time installation)
+    for type in $types; do
+        grep -q "^$type=" "$config_file" || echo "${type}=0" >> "$config_file"
+    done
+fi
 
 # set permissions
 chmod 755 $MODPATH/rmlwk.sh
