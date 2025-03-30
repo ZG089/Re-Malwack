c4f5d0d46e18ef7a32861e803e79a223e3dfc11d
ＺＧＸ０８９ <159061718+ZG089@users.noreply.github.com>
2025-03-30 18:57:47 +0200

    action: de-colorize text since text colors aren't desplayed properly

--- module/action.sh
+++ module/action.sh
@@ -18,19 +18,19 @@ echo "
 function april_fools() {
     touch $persist_dir/get_pranked
     clear
-    echo -e "\033[0;31m !!!!!!!!!!!!!!!!"
-    echo -e "\033[0;31m !!!  DANGER  !!!"
-    echo -e "\033[0;31m !!!!!!!!!!!!!!!!"
+    echo "!!!!!!!!!!!!!!!!"
+    echo "!!!  DANGER  !!!"
+    echo "!!!!!!!!!!!!!!!!"
     sleep 3
-    echo -e "\033[0;31m - Your device is now hacked."
+    echo "- Your device is now hacked."
     sleep 1.5
-    echo -e "\033[0;31m - Bricking the device in"
+    echo "- Bricking the device in"
     sleep 0.5
-    echo -e "\033[0;31m 3..."
+    echo "3..."
     sleep 1
-    echo -e "\033[0;31m 2..."
+    echo "2..."
     sleep 1
-    echo -e "\033[0;31m 1..."
+    echo "1..."
     sleep 3
     am start -a android.intent.action.VIEW -d "https://youtu.be/dQw4w9WgXcQ" 2>&1
     echo "- Happy April Fools!"
@@ -45,7 +45,7 @@ if [ "$(date +%m%d)" = "0401" ] && [ ! -f "$persist_dir/get_pranked" ]; then
     if [ "$(shuf -i 0-1 -n 1)" = "0" ]; then # Chance of happening: 50%
         sleep 2
         echo ""
-        echo -e "\033[0;31m - FATAL ERROR OCCURED!\033[0m\n"
+        echo "- FATAL ERROR OCCURED!\033[0m\n"
         sleep 2
         april_fools
     fi
