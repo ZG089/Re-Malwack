c12e7fa34aa109d6d9e780e43b7a5aea6a5e273a
Aidan <aidan@p0ntus.com>
2025-06-09 13:21:50 -0400

    website: design: use feature bubbles for shorter tagline, addl icons, footer and header design adjustments, swap dep icon

--- website/components/site-header.tsx
+++ website/components/site-header.tsx
@@ -1,22 +1,26 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { Github, Download, Coffee } from "lucide-react"
+import { Download, Coffee, Book } from "lucide-react"
+import { RiGithubLine } from "react-icons/ri"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
     <header className="border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="font-bold">Re-Malwack</span>
           </Link>
         </div>
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <nav className="flex items-center space-x-6">
-            <Link href="/guide" className="text-sm font-medium transition-colors hover:text-primary">
-              Guide
-            </Link>
+        <div className="flex flex-row items-center justify-end space-x-2 md:justify-end">
+          <nav className="flex items-center space-x-2">
+            <Button variant="ghost" className="h-8 w-full justify-start px-2 font-normal" asChild>
+              <Link href="/guide" className="text-sm font-medium transition-colors hover:text-primary">
+                <Book />
+                Guide
+              </Link>
+            </Button>
             <Button variant="ghost" className="h-8 w-full justify-start px-2 font-normal" asChild>
               <Link
                 href="https://buymeacoffee.com/zg089"
@@ -24,13 +28,13 @@ export function SiteHeader() {
                 rel="noreferrer"
                 className="flex items-center"
               >
-                <Coffee className="mr-2 h-4 w-4" />
+                <Coffee /> {/* sizing should be delegated to shadcn */}
                 Buy Me A Coffee
               </Link>
             </Button>
             <Button variant="outline" className="h-8" asChild>
               <Link href="/download" className="flex items-center">
-                <Download className="mr-2 h-4 w-4" />
+                <Download />
                 Download
               </Link>
             </Button>
@@ -38,10 +42,10 @@ export function SiteHeader() {
           <div className="flex items-center space-x-1">
             <ThemeToggle />
             <Link href="https://github.com/ZG089/Re-Malwack" target="_blank" rel="noreferrer">
-              <div className="inline-flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
-                <Github className="h-5 w-5" />
+              <Button variant="ghost" className="w-8 h-8">
+                <RiGithubLine size={16} />
                 <span className="sr-only">GitHub</span>
-              </div>
+              </Button>
             </Link>
           </div>
         </div>
