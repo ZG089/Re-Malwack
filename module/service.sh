aea8a3299dc8c8a7d8ecc3dd42d193badef086cc
ZG089 <159061718+ZG089@users.noreply.github.com>
2025-06-25 00:33:10 +0300

    module_service: fix, cleanup and rewrite

--- module/service.sh
+++ module/service.sh
@@ -1,22 +1,19 @@
-#!/bin/sh
+#!/system/bin/sh
 # This is the service script of Re-Malwack
 # It is executed during device boot
 
 # =========== Variables ===========
 MODDIR="${0%/*}"
-hosts_file="/system/etc/hosts"
+hosts_file="$MODDIR/system/etc/hosts"
 persist_dir="/data/adb/Re-Malwack"
 system_hosts="/system/etc/hosts"
-source $persist_dir/config.sh
-mkdir -p "$persist_dir/logs"
-rm -rf "$persist_dir/logs/"*
-
+last_mod=$(stat -c '%y' "$hosts_file" 2>/dev/null | cut -d'.' -f1) # Checks last modification date for hosts file
 # =========== Functions ===========
 
 # Function to check hosts file reset state
 is_default_hosts() {
-    [ ! -s "$1" ] && return 0
-    grep -v -E '^(127\.0\.0\.1|::1)[[:space:]]+localhost$' "$1" | grep -q '^0\.0\.0\.0' && return 1
+    grep -qvE '^#|^$' "$1" || return 1
+    grep -qvE '^127\.0\.0\.1 localhost$|^::1 localhost$' "$1" && return 1
     return 0
 }
 
@@ -39,6 +36,11 @@ function is_adblock_paused() {
 
 # =========== Main script logic ===========
 
+# preparation
+source $persist_dir/config.sh
+mkdir -p "$persist_dir/logs"
+rm -rf "$persist_dir/logs/"*
+
 # symlink rmlwk to manager path
 if [ "$KSU" = "true" ]; then
     [ -L "/data/adb/ksud/bin/rmlwk" ] || ln -sf "$MODDIR/rmlwk.sh" "/data/adb/ksud/bin/rmlwk" && log_message "symlink created at /data/adb/ksud/bin/rmlwk"
@@ -53,9 +55,9 @@ fi
 # System hosts count
 if is_default_hosts "$system_hosts"; then
     blocked_sys=0
-    log_message "System hosts file has default entries only.."
+    log_message "System hosts file has default entries only."
 else
-    blocked_sys=$(grep -c '^0\.0\.0\.0[[:space:]]' "$system_hosts" 2>/dev/null)
+    blocked_sys=$(grep -c '^0\.0\.0\.0[[:space:]]' "$system_hosts" > $persist_dir/counts/blocked_sys.count)
     blocked_sys=${blocked_sys:-0}
 fi
 log_message "System hosts entries count: $blocked_sys"
@@ -65,21 +67,22 @@ if is_default_hosts "$hosts_file"; then
     blocked_mod=0
     log_message "Module hosts file seems to be reset."
 else
-    blocked_mod=$(grep -c '^0\.0\.0\.0[[:space:]]' "$hosts_file" 2>/dev/null)
+    blocked_mod=$(grep -c '^0\.0\.0\.0[[:space:]]' "$hosts_file" > $persist_dir/counts/blocked_mod.count)
     blocked_mod=${blocked_mod:-0}
 fi
 log_message "Module hosts entries count: $blocked_mod"
 
 # Here goes the part where we actually determine module status
+
 if is_adblock_paused && [ "$blocked_mod" -gt 0 ]; then
     status_msg="Status: Ad-block is paused ⏸️"
 elif [ "$blocked_mod" -gt 10 ]; then
-    if [ "$blocked_mod" -ne "$blocked_sys" ]; then # Only for cases when mount breaks between module hosts and system hosts
+    if [ "$blocked_mod" -ne "$blocked_sys" ]; then # Only for cases if mount is broken between module hosts and system hosts
         status_msg="Status: Reboot required to apply changes 🔃 | Module blocks $blocked_mod domains, system hosts blocks $blocked_sys."
     else
         status_msg="Status: Protection is enabled ✅ | Blocking $blocked_mod domains | Last updated: $last_mod"
     fi
-elif [ "$blocked_sys" -eq 0 ]; then
+elif is_default_hosts "$system_hosts" && ! is_default_hosts "$hosts_file"; then
     status_msg="Status: Need to reboot once again 🔃 (If still showing same then report to developer)"
 else
     status_msg="Status: Protection is disabled due to reset ❌"
@@ -90,7 +93,7 @@ sed -i "s/^description=.*/description=$status_msg/" "$MODDIR/module.prop"
 log_message "$status_msg"
 
 # Check if auto-update is enabled
-if [ "$daily_update" = "1" ]; then
+if [ "$daily_update" = 1 ]; then
     # Check if crond is running
     if ! pgrep -x crond >/dev/null; then
         log_message "Auto-update is enabled, but crond is not running. Starting crond..."
