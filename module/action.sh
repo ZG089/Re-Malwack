686ae37985966848840bd66609a8fffa48a7b7b2
ZG089 <159061718+ZG089@users.noreply.github.com>
2025-12-22 15:21:53 +0200

    module_core (customize - action): final touches
    - Replacing ASCII text "Re-Malwack" with "Rmlwk" to be more short, reducing chances of text line wrap causing ASCII text to not to be displayed properly
    - Suppress failed dummy file removal
    - remove ANSI color stuff from easter egg message in action script

--- module/action.sh
+++ module/action.sh
@@ -1,35 +1,5 @@
 MODDIR="/data/adb/modules/Re-Malwack"
 persist_dir="/data/adb/Re-Malwack"
-function rmlwk_banner() {
-    banner1=$(cat <<'EOF'
-    ____             __  ___      __                    __            
-   / __ \___        /  |/  /___ _/ /      ______ ______/ /__          
-  / /_/ / _ \______/ /|_/ / __ `/ / | /| / / __ `/ ___/ //_/       
- / _, _/  __/_____/ /  / / /_/ / /| |/ |/ / /_/ / /__/ ,<             
-/_/ |_|\___/     /_/  /_/\__,_/_/ |__/|__/\__,_/\___/_/|_|           
-EOF
-)
-    banner2=$(cat <<'EOF'
-██████╗ ███████╗    ███╗   ███╗ █████╗ ██╗     ██╗    ██╗ █████╗  ██████╗██╗  ██╗
-██╔══██╗██╔════╝    ████╗ ████║██╔══██╗██║     ██║    ██║██╔══██╗██╔════╝██║ ██╔╝
-██████╔╝█████╗█████╗██╔████╔██║███████║██║     ██║ █╗ ██║███████║██║     █████╔╝ 
-██╔══██╗██╔══╝╚════╝██║╚██╔╝██║██╔══██║██║     ██║███╗██║██╔══██║██║     ██╔═██╗ 
-██║  ██║███████╗    ██║ ╚═╝ ██║██║  ██║███████╗╚███╔███╔╝██║  ██║╚██████╗██║  ██╗
-╚═╝  ╚═╝╚══════╝    ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
-EOF
-)
-
-    if command -v shuf >/dev/null 2>&1; then
-        random_index=$(shuf -i 1-2 -n 1)
-    else
-        random_index=$(( ($(date +%s) % 2) + 1 ))
-    fi
-
-    case $random_index in
-        1) echo "$banner1" ;;
-        2) echo "$banner2" ;;
-    esac
-}
 
 function abort() {
     echo "- $1"
@@ -68,14 +38,18 @@ if [ "$(date +%m%d)" = "0401" ] && [ ! -f "$persist_dir/get_pranked" ]; then
     if [ "$(shuf -i 0-1 -n 1)" = "0" ]; then # Chance of happening: 50%
         sleep 2
         echo ""
-        echo "- FATAL ERROR OCCURED!\033[0m\n"
+        echo "- FATAL ERROR OCCURED!"
         sleep 2
         april_fools
     fi
 else
-    rm -f $persist_dir/get_pranked    
+    rm -f $persist_dir/get_pranked 2>/dev/null
 fi
-rmlwk_banner       
+echo '    ____            __         __  '
+echo '   / __ \____ ___  / /      __/ /__'
+echo '  / /_/ / __ `__ \/ / | /| / / //_/'
+echo ' / _, _/ / / / / / /| |/ |/ / ,<   '
+echo '/_/ |_/_/ /_/ /_/_/ |__/|__/_/|_|  '
 echo " "
-echo " ====================================================================" 
+echo "============================================"
 sh "$MODDIR/rmlwk.sh" --update-hosts --quiet || abort "- Failed to update hosts."
