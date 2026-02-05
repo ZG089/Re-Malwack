7da860f3134a36e5315ae6fda445b1e4b2e7daba
ZG089 <159061718+ZG089@users.noreply.github.com>
2026-02-06 01:38:45 +0200

    module_addon: several improvements
    - do not write apk file in case of dl error
    - add a few safety checks
    - fixed APK installation failure handling
    - rewrite a few codes
    
    Co-authored-by: dnascorpionofficial <42230836+adarshjayakumarofficial@users.noreply.github.com>

--- module/rmlwk-app.sh
+++ module/rmlwk-app.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:/data/data/com.termux/files/usr/bin:$PATH
+
 # Replace with your repository details
 REPO_OWNER="itejo443"
 REPO_NAME="ReMalwack-app"
@@ -20,7 +21,7 @@ mkdir -p "$TEMP_DIR"
 
 download() {
 	if command -v curl > /dev/null 2>&1; then
-		curl --connect-timeout 10 -Ls "$1"
+		curl --connect-timeout 10 -Lsf "$1"
     else
 		busybox wget -T 10 --no-check-certificate -qO - "$1"
     fi
@@ -40,17 +41,30 @@ fi
 echo "[i] Latest APK URL: $latest_release_url"
 
 # Download the APK file using download()
-download "$latest_release_url" > "$APK_PATH" || echo "[✗] Failed to download the APK."
+if ! download "$latest_release_url" > "$APK_PATH"; then
+	echo "[✗] APK download failed."
+	exit 1
+fi
+
+# Safety check (empty / broken file)
+if [ ! -s "$APK_PATH" ]; then
+	echo "[✗] Downloaded APK is empty or corrupted."
+	exit 1
+fi
 
 # Install the APK as a user app
-pm install -r "$APK_PATH" 2>&1 </dev/null | cat
-
-# Cooldown to ensure installation completes
-sleep 3
+if ! pm install -r "$APK_PATH" >/dev/null 2>&1; then
+	echo "[✗] Installation failed!"
+	mkdir -p /sdcard/Download/rmlwk-app
+	cp -f "$APK_PATH" /sdcard/Download/rmlwk-app/app.apk
+	echo "[i] Please manually install app from /sdcard/Download/rmlwk-app"
+	exit 1
+fi
 
 # Check if the installation was successful by verifying the app's presence
-if ! pm path me.itejo443.remalwack > /dev/null 2>&1 ; then
-	echo "[✓] Re-Malwack QuickTile Add-on has been Installed Successfully!"
+if pm path "$APP_PACKAGE" >/dev/null 2>&1; then
+	echo "[✓] Re-Malwack QuickTile Add-on installed successfully!"
+	# Create self-delete script to remove the app if the module is uninstalled
 	mkdir -p "$SERVICE_DIR"
 	cat > "$SELF_DELETE" << 'EOF'
 #!/system/bin/sh
@@ -59,19 +73,16 @@ MODULE_DIR="/data/adb/modules/Re-Malwack"
 APP_PKG="me.itejo443.remalwack"
 SELF="$0"
 
-if [ ! -d "$MODULE_DIR" ] && pm path $APP_PKG; then
-    pm uninstall "$APP_PKG"
-    rm -f "$SELF"
+if [ ! -d "$MODULE_DIR" ] && pm path "$APP_PKG" >/dev/null 2>&1; then
+	pm uninstall "$APP_PKG"
+	rm -f "$SELF"
 fi
 exit 0
 EOF
 	chmod 755 "$SELF_DELETE"
 else
-	echo "[✗] Failed to install apk!"
-	# Save the APK to the failsafe directory if devpts hooks fail
-	mkdir -p /sdcard/Download/rmlwk-app
-	cp -f "$APK_PATH" /sdcard/Download/rmlwk-app/app.apk
-	echo "[i] Please manually install app from /sdcard/Download/rmlwk-app"
+	echo "[✗] App installation verification failed."
+	exit 1
 fi
 
 # Clean up
