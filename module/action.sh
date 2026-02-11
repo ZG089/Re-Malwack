33d31df0e7be223828c342f04c85916b7aaeed80
ZG089 <159061718+ZG089@users.noreply.github.com>
2026-02-11 18:23:42 +0200

    module_core(feat): Action script mode
    - Customize the purpose of module action button, pausing/resuming protection and updating hosts are only available for now

--- module/action.sh
+++ module/action.sh
@@ -4,6 +4,12 @@
 MODDIR="/data/adb/modules/Re-Malwack"
 persist_dir="/data/adb/Re-Malwack"
 
+# Path to config file
+CONFIG="$persist_dir/config.sh"
+
+# Read action_mode
+ACTION_MODE="$(grep '^action_mode=' "$CONFIG" 2>/dev/null | cut -d= -f2)"
+
 # ====== Functions ======
 
 # 1 - Abort function
@@ -60,4 +66,10 @@ echo ' / _, _/ / / / / / /| |/ |/ / ,<   '
 echo '/_/ |_/_/ /_/ /_/_/ |__/|__/_/|_|  '
 echo " "
 echo "==================================="
-sh "$MODDIR/rmlwk.sh" --update-hosts --quiet || abort "- Failed to update hosts."
+if [ "$ACTION_MODE" = "1" ]; then
+    # Pause & Resume
+    sh "$MODDIR/rmlwk.sh" -as || abort "- Failed to Resume/Pause"
+else
+    # Update Hosts
+    sh "$MODDIR/rmlwk.sh" --update-hosts --quiet || abort "- Failed to update hosts."
+fi
